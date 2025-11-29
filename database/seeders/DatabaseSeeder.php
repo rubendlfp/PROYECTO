7a5ca21dddf9cd29d86a5798383696d8db305c96
Rubén <rdelafuente@liderit.es>
2025-11-29 17:33:44 +0100

    Disable ProductoSeeder - products inserted in migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             UserSeeder::class,
-            ProductoSeeder::class,
+            // ProductoSeeder::class, // Los productos se insertan en la migración
             CompraventaSeeder::class,
         ]);
     }
